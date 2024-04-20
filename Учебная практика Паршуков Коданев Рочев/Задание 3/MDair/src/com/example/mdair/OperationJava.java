1b069343428f1c56a38592f8e6badb0b5ed4edbf
SBonkS <164881140+SBonkS@users.noreply.github.com>
2024-04-20 10:46:54 +0300

    Add files via upload

---
+++ Учебная практика Паршуков Коданев Рочев/Задание 3/MDair/src/com/example/mdair/OperationJava.java
@@ -0,0 +1,66 @@
+package com.example.mdair;
+
+
+import android.util.Log;
+
+
+
+public class OperationJava {
+	private  static final String TAG = "OperationClass";
+
+
+	private  static final ShareFiles = "ShareClass" ;
+	//поделиться данными
+
+
+public class UpLoadFiles {
+	private static final String TAG = "UpLoadClass";
+	//загрузка файлов
+	
+	
+	public void ChangeFiles() {
+	Log.d(TAG, "Выбор данных");
+	//Выбор данных
+	}
+	public void CloudSynch() {
+		Log.d(TAG, "Синхронизация с облаком");
+		//Синхронизация с облаком
+	}
+	public void FileFormat() {
+		Log.d(TAG, "Установка фораматов файлов");
+		//Установка фораматов файлов
+	}
+	public void SaveToCloud() {
+		Log.d(TAG, "Сохранение файлов в облако");
+		//Сохранение файлов в облако
+	}
+	
+}
+
+public class DeleteFile {
+	private static final String TAG = "DeleteClass";
+	//Удаление файлов
+	
+	
+	public void ChangeFiles() {
+	Log.d(TAG, "Выбор данных");
+	//Выбор данных
+	}
+	public void CloudSynch() {
+		Log.d(TAG, "Синхронизация с облаком");
+		//Синхронизация с облаком
+	}
+	public void DeleteFiles() {
+		Log.d(TAG, "Удаление файлов");
+		//Удаление файлов
+	}
+	
+	
+}
+
+
+
+
+
+
+}
